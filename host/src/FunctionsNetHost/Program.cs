751969de01ac39b85e22b622a17fa7da171b5c9a
Shyju Krishnankutty <connectshyju@gmail.com>
2023-05-10 14:34:09 -0700

    Minimal workling version (AOT published) which establishes GRPC conn

--- host/src/FunctionsNetHost/Program.cs
+++ host/src/FunctionsNetHost/Program.cs
@@ -1,8 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-using System.CommandLine;
-using FunctionsNetHost.Grpc;
+﻿using System.CommandLine;
 
 namespace FunctionsNetHost
 {
@@ -10,54 +6,34 @@ namespace FunctionsNetHost
     {
         static async Task Main(string[] args)
         {
-            try
-            {
-                Logger.LogInfo("Starting FunctionsNetHost");
+            Logger.Log("Starting FunctionsNetHost V2v 1138");
 
-                var workerStartupOptions = await GetStartupOptionsFromCmdLineArgs(args);
+            GrpcWorkerStartupOptions grpcOptions = new GrpcWorkerStartupOptions() ;
 
-                using var appLoader = AppLoader.Instance;
-                var grpcClient = new GrpcClient(workerStartupOptions, appLoader);
 
-                await grpcClient.InitAsync();
-            }
-            catch (Exception exception)
-            {
-                Logger.LogInfo($"An error occurred while running FunctionsNetHost.{exception}");
-            }
-        }
-
-        private static async Task<GrpcWorkerStartupOptions> GetStartupOptionsFromCmdLineArgs(string[] args)
-        {
-            var hostOption = new Option<string>("--host");
+            var hostOption = new Option<string>( "--host");
             var portOption = new Option<int>("--port");
             var workerOption = new Option<string>("--workerId");
-            var grpcMsgLengthOption = new Option<int>("--grpcMaxMessageLength");
-            var requestIdOption = new Option<string>("--requestId");
 
             var rootCommand = new RootCommand();
             rootCommand.AddOption(portOption);
             rootCommand.AddOption(hostOption);
             rootCommand.AddOption(workerOption);
-            rootCommand.AddOption(grpcMsgLengthOption);
-            rootCommand.AddOption(requestIdOption);
 
-            var workerStartupOptions = new GrpcWorkerStartupOptions();
+            rootCommand.SetHandler((host, port, workerId) =>
+            {
+                grpcOptions.Host = host;
+                grpcOptions.Port = port;
+                grpcOptions.WorkerId = workerId;
+                grpcOptions.GrpcMaxMessageLength = int.MaxValue;
+            },
+            hostOption, portOption, workerOption);
 
-            rootCommand.SetHandler((host, port, workerId, grpcMsgLength, requestId) =>
-                {
-                    workerStartupOptions.Host = host;
-                    workerStartupOptions.Port = port;
-                    workerStartupOptions.WorkerId = workerId;
-                    workerStartupOptions.GrpcMaxMessageLength = grpcMsgLength;
-                    workerStartupOptions.RequestId = requestId;
-                },
-                hostOption, portOption, workerOption, grpcMsgLengthOption, requestIdOption);
+            await rootCommand.InvokeAsync(args);
 
-            var argsWithoutExecutableName = args.Skip(1).ToArray();
-            await rootCommand.InvokeAsync(argsWithoutExecutableName);
+            var client = new MyClient(grpcOptions);
 
-            return workerStartupOptions;
+            await client.InitAsync();
         }
     }
 }
