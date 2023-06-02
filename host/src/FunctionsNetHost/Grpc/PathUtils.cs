aa88d654db3e22cd502b3c08629c54b1a12d6ef6
Shyju Krishnankutty <connectshyju@gmail.com>
2023-06-01 19:46:19 -0700

    111 - Logging JsonString inside GetApplicationExePath

--- host/src/FunctionsNetHost/Grpc/PathUtils.cs
+++ host/src/FunctionsNetHost/Grpc/PathUtils.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace FunctionsNetHost.Grpc
@@ -14,6 +15,7 @@ namespace FunctionsNetHost.Grpc
         /// <param name="applicationDirectory">The FunctionAppDirectory value from environment reload request.</param>
         internal static string? GetApplicationExePath(string applicationDirectory)
         {
+            string jsonString = string.Empty;
             try
             {
                 var workerConfigPath = Path.Combine(applicationDirectory, "worker.config.json");
@@ -26,21 +28,26 @@ namespace FunctionsNetHost.Grpc
                     throw new FileNotFoundException($"worker.config.json file not found", fileName: workerConfigPath);
                 }
 
-                var jsonString = File.ReadAllText(workerConfigPath);
+                jsonString = File.ReadAllText(workerConfigPath);
                 var workerConfigJsonNode = JsonNode.Parse(jsonString)!;
                 var executableName = workerConfigJsonNode["description"]?["defaultWorkerPath"]?.ToString();
 
                 if (executableName == null)
                 {
-                    Logger.LogInfo($"Invalid worker configuration. description > defaultWorkerPath property value is null.");
+                    Logger.LogInfo($"Invalid worker configuration. description > defaultWorkerPath property value is null. jsonString:{jsonString}");
                     return null;
                 }
 
                 return Path.Combine(applicationDirectory, executableName);
             }
+            catch (JsonException ex)
+            {
+                Logger.LogInfo($"Error parsing JSON in GetApplicationExePath.{ex}. jsonString:{jsonString}");
+                return null;
+            }
             catch (Exception ex)
             {
-                Logger.LogInfo($"Error in GetApplicationExePath.{ex}");
+                Logger.LogInfo($"Error in GetApplicationExePath.{ex}. jsonString:{jsonString}");
                 return null;
             }
         }
--- host/src/FunctionsNetHost/Program.cs
+++ host/src/FunctionsNetHost/Program.cs
@@ -12,7 +12,7 @@ namespace FunctionsNetHost
         {
             try
             {
-                Logger.LogInfo("Starting FunctionsNetHost");
+                Logger.LogInfo("Starting FunctionsNetHost202306011945");
                 Environment.SetEnvironmentVariable(EnvironmentSettingNames.FunctionsNetHostTrace, "1");
 
                 var workerStartupOptions = await GetStartupOptionsFromCmdLineArgs(args);
